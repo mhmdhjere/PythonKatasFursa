0f518607b84d5be70934f76046f3fba1300297b5
mhmdhjere <mhmd.hjere.98@gmail.com>
2025-05-01 21:43:40 +0300

    correcting the class name

--- katas/test/test_is_unique_str.py
+++ katas/test/test_is_unique_str.py
@@ -2,7 +2,7 @@ import unittest
 from katas.is_unique_str import is_unique
 
 
-class TestHelloWorld(unittest.TestCase):
+class TestIsUniqueStr(unittest.TestCase):
     def test_is_unique_str(self):
         self.assertEqual(is_unique("Hello"), False)
         self.assertEqual(is_unique("World"), True)
