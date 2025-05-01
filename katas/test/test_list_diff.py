65a5bc4af61fba32c9df3c0bae57639a239f594f
mhmdhjere <mhmd.hjere.98@gmail.com>
2025-05-01 21:46:02 +0300

    unit tests for list_diff

---
+++ katas/test/test_list_diff.py
@@ -0,0 +1,9 @@
+import unittest
+from katas.list_diff import find_difference
+
+
+class TestHelloWorld(unittest.TestCase):
+    def test_list_diff(self):
+        self.assertEqual(find_difference([10, 3, 5, 6, 20, -2]), 22)
+        self.assertEqual(find_difference([]), 0)
+        self.assertEqual(find_difference([5]),0)
